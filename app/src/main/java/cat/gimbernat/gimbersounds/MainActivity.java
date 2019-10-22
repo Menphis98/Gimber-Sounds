65361282ff806528030c8994f5ebd5e4a902d299
Joan <manyajoan@gmail.com>
2019-10-22 19:47:01 +0200

    1r Commit

---
+++ app/src/main/java/cat/gimbernat/gimbersounds/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.gimbernat.gimbersounds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
