23dc7e385907e12f0537b64b25dd7a304c0d02bc
Joan <manyajoan@gmail.com>
2019-10-29 19:07:35 +0100

    Canviar el nom a B

---
+++ app/src/main/java/cat/gimbernat/gimbersounds/scenes/boot/BootActivity.java
@@ -0,0 +1,16 @@
+package cat.gimbernat.gimbersounds.scenes.boot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cat.gimbernat.gimbersounds.R;
+
+public class BootActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
