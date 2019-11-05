6fa2dd9122c5540d3c22d146a5dce3ec019864c5
Joan <manyajoan@gmail.com>
2019-11-05 19:35:33 +0100

    Closing #9
    He implementado el Boot activity

--- app/src/main/java/cat/gimbernat/gimbersounds/scenes/boot/BootActivity.java
+++ app/src/main/java/cat/gimbernat/gimbersounds/scenes/boot/BootActivity.java
@@ -2,15 +2,43 @@ package cat.gimbernat.gimbersounds.scenes.boot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import cat.gimbernat.gimbersounds.DataSources.SessionDataSource;
 import cat.gimbernat.gimbersounds.R;
+import cat.gimbernat.gimbersounds.scenes.categoriesList.CategoriesListActivity;
+import cat.gimbernat.gimbersounds.scenes.welcome.WelcomeActivity;
 
-public class BootActivity extends AppCompatActivity {
-    
+interface IbootActivity{
+     void navigateToPublic();
+     void navigateToPrivate();
+ }
+public class BootActivity extends AppCompatActivity implements IbootActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(SessionDataSource.shared.isUserLogedIn()){
+            this.navigateToPrivate();
+        }else {
+            this.navigateToPublic();
+        }
+    }
+
+    @Override
+    public void navigateToPublic() {
+        Intent intent = new Intent(BootActivity.this, WelcomeActivity.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        BootActivity.this.startActivity(intent);
+
+    }
+
+    @Override
+    public void navigateToPrivate() {
+        Intent intent = new Intent(BootActivity.this, CategoriesListActivity.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        BootActivity.this.startActivity(intent);
     }
 }
