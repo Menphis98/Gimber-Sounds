af76e65b31a3ef192c610978ca1e63572a697ce4
Joan <manyajoan@gmail.com>
2019-11-12 20:09:26 +0100

    He añadido Picasso
    Hemos puesto una URL y tenemos que ver las imagenes

--- app/src/main/java/cat/gimbernat/gimbersounds/scenes/categoriesList/CategoriesListActivity.java
+++ app/src/main/java/cat/gimbernat/gimbersounds/scenes/categoriesList/CategoriesListActivity.java
@@ -3,6 +3,9 @@ package cat.gimbernat.gimbersounds.scenes.categoriesList;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import cat.gimbernat.gimbersounds.R;
 
@@ -12,5 +15,11 @@ public class CategoriesListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories_list);
+
+  String url = "https://firebasestorage.googleapis.com/v0/b/gimbersounds.appspot.com/o/content%2Fimagenes%2Fbosque.jpg?alt=media&token=12f2eb87-894b-40c4-add2-0f9db63179a3";
+
+
+        Picasso.get().load(url).into((ImageView) this.findViewById(R.id.imageView5));
+
     }
 }
