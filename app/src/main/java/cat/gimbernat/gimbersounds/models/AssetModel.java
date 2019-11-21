0fee7cc9e95bd5d2672a1cc689ce96034367aff2
Adrià <asolerc@campus.eug.es>
2019-11-21 19:35:01 +0100

    AssetModel
    Actualización del modelo assets con metodos getters

--- app/src/main/java/cat/gimbernat/gimbersounds/models/AssetModel.java
+++ app/src/main/java/cat/gimbernat/gimbersounds/models/AssetModel.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
 
 public class AssetModel {
 
-    public String id;
-    public String url;
-    public ArrayList category;
+    private String id;
+    private String url;
+    private ArrayList category;
 
     public AssetModel(String id, String url, ArrayList category) {
         this.id = id;
         this.url = url;
         this.category = category;
+    }
+
+    public String getID(){
+        return this.id;
+    }
+
+    public String getUrl(){
+        return this.url;
+    }
+
+    public ArrayList getCategory(){
+        return this.category;
     }
 }
