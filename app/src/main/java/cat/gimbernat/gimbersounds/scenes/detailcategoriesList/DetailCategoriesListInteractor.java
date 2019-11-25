6073889e05a1935c3d0731e0e6a4e799aeeeca04
David <davidfeijoovalles@gmail.com>
2019-11-25 17:20:05 +0100

    DetailActivity
    hemos implementado la vista que vemos cuando clicamos encima de una categoría salen todas las fotos disponibles de esa categoría

--- app/src/main/java/cat/gimbernat/gimbersounds/scenes/detailcategoriesList/DetailCategoriesListInteractor.java
+++ app/src/main/java/cat/gimbernat/gimbersounds/scenes/detailcategoriesList/DetailCategoriesListInteractor.java
@@ -1,4 +1,32 @@
 package cat.gimbernat.gimbersounds.scenes.detailcategoriesList;
 
-public class DetailCategoriesListInteractor {
-}
+import android.util.Log;
+
+import cat.gimbernat.gimbersounds.DataSources.AssetsDataSource;
+import cat.gimbernat.gimbersounds.helpers.Callback;
+import cat.gimbernat.gimbersounds.models.AssetModel;
+import cat.gimbernat.gimbersounds.scenes.detailcategoriesList.interfaces.IDetailCategoriesListInteractor;
+
+import java.util.ArrayList;
+
+public class DetailCategoriesListInteractor implements IDetailCategoriesListInteractor{
+
+    @Override
+    public void subscribeForAssets(final Callback callback) {
+        //Interface IGalleryInteractor
+            AssetsDataSource.shared.subscribe(new Callback() {
+                @Override
+                public void onSuccess(Object responseObject) {
+                    ArrayList<AssetModel> assets = (ArrayList<AssetModel>) responseObject;
+                    callback.onSuccess(assets);
+                }
+
+                @Override
+                public void onError() {
+                    Log.d("debug", "error");
+                    callback.onError();
+                }
+            });
+        }
+    }
+
--- app/src/main/java/cat/gimbernat/gimbersounds/scenes/detailcategoriesList/interfaces/IDetailCategoriesListActivity.java
+++ app/src/main/java/cat/gimbernat/gimbersounds/scenes/detailcategoriesList/interfaces/IDetailCategoriesListActivity.java
@@ -1,4 +1,10 @@
 package cat.gimbernat.gimbersounds.scenes.detailcategoriesList.interfaces;
 
+import cat.gimbernat.gimbersounds.models.AssetModel;
+
 public interface IDetailCategoriesListActivity {
+    void navigateToDetail(AssetModel assetModel);
+    void setAdapterForGrid();
+    void showSpinner();
+    void hideSpinner();
 }
